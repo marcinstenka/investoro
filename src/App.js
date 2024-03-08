3de6760e9ce5ee2254e7951d0ea7bee767509372
marcinstenka <mastenka@wp.pl>
2024-03-08 23:33:04 +0100

    json data added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<img
+				src='https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+				alt=''
+			/>
+		</div>
+	);
 }
 
 export default App;
