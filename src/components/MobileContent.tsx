8d9e2382d674d21f40943131deb9e37868086e71
marcinstenka <mastenka@wp.pl>
2024-03-09 17:06:45 +0100

    Mobile content

--- src/components/MobileContent.tsx
+++ src/components/MobileContent.tsx
@@ -1,30 +1,46 @@
 import React from 'react';
+import { Navigation } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { Link } from 'react-router-dom';
 import { Advertisement } from '../types/types';
-import { Swiper, SwiperSlide } from 'swiper/react';
-
 import 'swiper/css';
+import 'swiper/css/navigation';
+
 type MobileContentProps = {
 	data: Advertisement[];
 };
 export default function MobileContent({ data }: MobileContentProps) {
 	return (
 		<div className='mobile_content'>
-			<Swiper spaceBetween={50} slidesPerView={3}>
+			<Swiper
+				navigation
+				modules={[Navigation]}
+				slidesPerView={1}
+				spaceBetween={10}
+				breakpoints={{
+					320: {
+						slidesPerView: 2,
+					},
+					600: {
+						slidesPerView: 3,
+					},
+				}}
+			>
 				{data &&
 					data.map((item) => (
 						<SwiperSlide>
-							<Link
-								to={`/advertisement/${item.id}`}
-								className='item'
-								key={item.id}
-							>
-								<h1 className='item_title'>{item.title}</h1>
-								<div className='item_photo'>
+							<div className='item' key={item.id}>
+								<Link className='item_photo' to={`/advertisement/${item.id}`}>
 									<img src={item.photo} alt={item.description} />
+								</Link>
+								<div className='item_info'>
+									<h1 className='item_title'>{item.title}</h1>
+									<div className='item_stats'>
+										<p className='item_price'>{item.price} zł</p>
+										<p className='item_price'>{item.area} m²</p>
+									</div>
 								</div>
-								<p className='item_price'>{item.price}</p>
-							</Link>
+							</div>
 						</SwiperSlide>
 					))}
 			</Swiper>
